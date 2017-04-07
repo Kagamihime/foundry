ff97e60d583e2c7470312902357509097b80f1eb
Kagamihime <chenyensu0@gmail.com>
2017-04-07 21:58:30 +0200

    Added an example to do basic tests.

---
+++ examples/printable_test.rs
@@ -0,0 +1,21 @@
+extern crate rlife;
+
+use std::process;
+
+use rlife::Grid;
+
+fn main() {
+    let mut grid = match Grid::load_life_file(&String::from("test.life")) {
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        },
+        Ok(grid) => grid,
+    };
+
+    println!("{}", grid);
+    for _ in 0..5 {
+        grid = grid.next_gen();
+        println!("{}", grid);
+    }
+}
